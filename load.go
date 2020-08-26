32f5f8ea42acd78bbd284bdfa58be461a2cc8de9
Pantonshire <tom@tomandtally.co.uk>
2020-08-26 18:57:37 +0100

    Function to load template from file

---
+++ load.go
@@ -0,0 +1,43 @@
+package marmot
+
+import (
+    "io/ioutil"
+    "regexp"
+    "strings"
+)
+
+type tpldata struct {
+    content  []byte
+    extends  []string
+    includes []string
+}
+
+var (
+    reExtend  = regexp.MustCompile(`(?s){{\s*extend\s[^{}]*}}`)
+    reInclude = regexp.MustCompile(`(?s){{\s*include\s[^{}]*}}`)
+)
+
+func loadTemplate(path string) (data tpldata, err error) {
+    content, err := ioutil.ReadFile(path)
+    if err != nil {
+        return data, err
+    }
+
+    if match := reExtend.FindIndex(content); match != nil {
+        data.extends = parseDependencies(string(content[match[0]:match[1]]))
+        content = append(content[:match[0]], content[match[1]:]...)
+    }
+
+    if match := reInclude.FindIndex(content); match != nil {
+        data.includes = parseDependencies(string(content[match[0]:match[1]]))
+        content = append(content[:match[0]], content[match[1]:]...)
+    }
+
+    data.content = content
+
+    return data, nil
+}
+
+func parseDependencies(dependencyStatement string) []string {
+    return strings.Fields(dependencyStatement[2 : len(dependencyStatement)-2])[1:]
+}
